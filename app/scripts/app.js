75eb6f4a172ba8921a53817938ff36e64cefc36a
seiya kawamura <seiya.kawamura@gmail.com>
2016-11-27 23:38:36 +0900

    fetchを導入

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,3 +1,5 @@
+'use babel';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from '../views/app.jsx';
--- app/views/app.jsx
+++ app/views/app.jsx
@@ -2,57 +2,34 @@
 
 import React from 'react';
 import { Button, Card, CardText } from 'react-mdl';
-import { YouTube } from 'react-youtube'
-import Reactotron from 'reactotron-react-js'
-import '../../ReactotronConfig'
-
-const videoIdA = 'XxVg_s8xAms';
-const videoIdB = '-DX3vJiqxm4';
+import { YouTube } from 'react-youtube';
+import 'whatwg-fetch';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-
-    this.state = {
-      videoId: videoIdA,
-      player: null,
-    };
-
-    this.onReady = this.onReady.bind(this);
-    this.onChangeVideo = this.onChangeVideo.bind(this);
-    this.onPlayVideo = this.onPlayVideo.bind(this);
-    this.onPauseVideo = this.onPauseVideo.bind(this);
+    this.state = { videoId: 'XxVg_s8xAms' };
+    this.apiTest = this.apiTest.bind(this);
   }
 
-  onReady(event) {
-    console.log(`YouTube Player object for videoId: "${this.state.videoId}" has been saved to state.`); // eslint-disable-line
-    this.setState({
-      player: event.target,
-    });
-  }
-
-  onPlayVideo() {
-    this.state.player.playVideo();
-  }
-
-  onPauseVideo() {
-    this.state.player.pauseVideo();
-  }
-
-  onChangeVideo() {
-    this.setState({
-      videoId: this.state.videoId === videoIdA ? videoIdB : videoIdA,
-    });
-  }
+  apiTest(){
+    const url = 'https://echo.paw.cloud/';
+    fetch(url, {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+    }).then((response) => {
+      console.log(this);
+      this.setState({ videoId: response.status });
+    }, function(error) {
+      error.message
+    })
+  };
 
   render() {
     return (
       <div>
         <h1>Session</h1>
-        <YouTube videoId={this.state.videoId} onReady={this.onReady} />
-        <button onClick={this.onPlayVideo}>Play</button>
-        <button onClick={this.onPauseVideo}>Pause</button>
-        <button onClick={this.onChangeVideo}>Change Video</button>
+        <Button raised colored onClick={this.apiTest}>{this.state.videoId}</Button>
       </div>
     )
   }
