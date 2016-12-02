b0b56e770c6906613d59aab4f935021a6fd7caa9
seiya kawamura <seiya.kawamura@gmail.com>
2016-12-03 01:57:35 +0900

    photon導入

--- app/views/app.jsx
+++ app/views/app.jsx
@@ -1,7 +1,7 @@
 'use babel';
 
 import React from 'react';
-import { Button, Textfield, Card, CardTitle, CardActions } from 'react-mdl';
+// import { Button, Textfield, Card, CardTitle, CardActions } from 'react-mdl';
 import { YouTube } from 'react-youtube';
 import firebase from 'firebase';
 import 'whatwg-fetch';
@@ -13,7 +13,7 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      videoId: 'qQLUiZsEnnA',
+      videoId: '',
       searchText: '',
       searchResult: [],
       searchResultNum: '',
@@ -32,8 +32,8 @@ export default class App extends React.Component {
     return true;
   }
 
-  onClickSetQue(videoId) {
-    this.setState({que: this.state.que.puch(videoId) ,videoId});
+  onClickSetQue(result) {
+    this.setState({que: [...this.state.que, result] ,videoId: result.videoId});
   }
 
   onChangeSearchText(value) {
@@ -64,41 +64,70 @@ export default class App extends React.Component {
 
   render() {
     const searchResultNode = this.state.searchResult.map((result, i) => (
-      <Card
-        key={i}
-        shadow={0}
-        style={{width: '256px', height: '256px', background: `url(${result.thumbnail.url}) center / cover`, margin: 'auto'}}
-        onClick={() => this.onClickSetQue(result.videoId)}
-      >
-        <CardTitle expand />
-        <CardActions style={{height: '52px', padding: '16px', background: 'rgba(0,0,0,0.2)'}}>
-        <span style={{color: '#fff', fontSize: '14px', fontWeight: '500'}}>
-            {result.title}
-         </span>
-        </CardActions>
-      </Card>
+      <ul key={i} className="list-group" onClick={() => this.onClickSetQue(result)}>
+        <li className="list-group-item">
+          <img className="img-circle media-object pull-left" src={result.thumbnail.url} width="32" height="32"></img>
+          <div className="media-body">
+            <strong>{result.title}</strong>
+          </div>
+        </li>
+      </ul>
+    ));
+
+    const queNode = this.state.que.map((result, i) => (
+      <ul key={i} className="list-group">
+        <li className="list-group-item">
+          <img className="img-circle media-object pull-left" src={result.thumbnail.url} width="32" height="32"></img>
+          <div className="media-body">
+            <strong>{result.title}</strong>
+          </div>
+        </li>
+      </ul>
     ));
 
     return (
-      <div>
-        <h1>Session</h1>
-        <div>
-          <Textfield
-            type='text'
-            placeholder='type something you want to share'
-            onChange={(e) => this.onChangeSearchText(e.target.value)}
-            onKeyPress={this.onKeyPress}
-            value={this.state.searchText}
-            label="Expandable Input"
-            expandable
-            expandableIcon="search"
-          />
+      <div className="window">
+        <div className="window-content">
+          <div className="pane-group">
+            <div className="pane">
+              <h5 className="nav-group-title">
+                <span className="icon icon-music"></span>
+                Up Coming
+              </h5>
+              {queNode}
+            </div>
+
+            <div className="pane">
+              <iframe
+                width="560"
+                height="315"
+                src={videoUrl(this.state.videoId)}
+              ></iframe>
+            </div>
+
+            <div className="pane-sm sidebar">
+              <ul className="list-group">
+                <li className="list-group-header">
+                  <span className="icon icon-search"></span>
+                  <input
+                    className="form-control"
+                    type="text"
+                    placeholder="Search for someone"
+                    onChange={(e) => this.onChangeSearchText(e.target.value)}
+                    onKeyPress={this.onKeyPress}
+                    value={this.state.searchText}
+                  >
+                  </input>
+                </li>
+              </ul>
+              <h5 className="nav-group-title">
+                <span className="icon icon-menu"></span>
+                Search Result
+              </h5>
+              {searchResultNode}
+            </div>
+          </div>
         </div>
-        <div>
-          <p>{this.state.searchResultNum}</p>
-          {searchResultNode}
-        </div>
-        <iframe width="560" height="315" src={videoUrl(this.state.videoId)} frameBorder="0"></iframe>
       </div>
     );
   }
