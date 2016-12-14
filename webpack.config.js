e167a770abbcb2c3a00b440a7bcbfdfa2639d6d5
seiya <seiya.kawamura@gmail.com>
2016-12-14 15:20:28 +0900

    不要なnpmを削除

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,2 @@
-module.exports = {
-  context: __dirname + './app',
-  entry: {
-    'application': './main',
-  },
-  output: {
-    path: __dirname + '/dist/js',
-    filename: '[name].js'
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: "babel",
-        query:{
-          presets: ['react', 'es2015']
-        }
-      }
-    ]
-  }
-};
+require('babel-core/register'); // development.jsでES6を使えるようにする
+module.exports = require('./development');
