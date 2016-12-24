ce1016bf4aec63ce16d321a99260b9257a074fcc
seiya kawamura <seiya.kawamura@gmail.com>
2016-12-24 23:23:07 +0900

    webpackにcssとscssを追加

--- webpack.config.js
+++ webpack.config.js
@@ -1,106 +1,124 @@
 const webpack = require('webpack');
 const path = require('path');
-const JsonpTemplatePlugin = webpack.JsonpTemplatePlugin;
-const FunctionModulePlugin = require('webpack/lib/FunctionModulePlugin');
 const NodeTargetPlugin = require('webpack/lib/node/NodeTargetPlugin');
 const ExternalsPlugin = webpack.ExternalsPlugin;
 
 const DEBUG = !process.argv.includes('--release');
-const bundles = path.resolve(__dirname, 'bundles');
-const entries = [
-  { key: 'index', file: './renderer/components/index.jsx' },
+const bundles = (assetType) => path.resolve(__dirname, `app/assets/${assetType}/bundles`);
+
+const Scripts = [
+  { key: 'index', file: './index.jsx' },
 ];
-const entry = {};
-entries.forEach(e => Object.assign(entry,
-  { [e.key]: [e.file] }
-));
 
-const config = {
-  cache: DEBUG,
-  context: `${__dirname}/app`,
-  entry,
-  output: {
-    path: bundles,
-    filename: '[name].bundle.js',
-    sourceMapFilename: '[name].map',
-  },
+const Styles = [
+  { key: 'base', file: './base.css' },
+  { key: 'normalize', file: './normalize.css' },
+];
 
-  target: 'electron',
+const entryForScripts = {};
+const entryForStyles = {};
 
-  resolve: {
-    extensions: ['', '.js', '.jsx', '.json'],
-    root: [
-      `${__dirname}/node_modules`,
-      path.resolve('./app'),
-    ],
-    packageMains: ['webpack', 'browser', 'web', 'browserify', ['jam', 'main'], 'main'],
-  },
+Scripts.forEach(script => Object.assign(entryForScripts, { [script.key]: [script.file] }));
+Styles.forEach(style => Object.assign(entryForStyles, { [style.key]: [style.file] }));
 
-  module: {
-    preLoaders: [{
-      test: /\.jsx$|\.js$/,
-      loader: 'eslint-loader',
-      exclude: /node_modules/,
-    }],
-    loaders: [
-      {
-        test: /\.jsx$|\.js$/,
-        loader: 'babel-loader',
-        exclude: /node_modules/,
-        query: {
-          presets: ['es2015', 'react'],
+const config = [
+  // jsx関連
+  {
+    cache: DEBUG,
+    context: `${__dirname}/app/renderer/components`,
+    entry: entryForScripts,
+    output: {
+      path: bundles('scripts'),
+      filename: '[name].bundle.js',
+      sourceMapFilename: '[name].map',
+    },
+    target: 'electron',
+    resolve: {
+      extensions: ['', '.js', '.jsx', '.json'],
+      root: [
+        `${__dirname}/node_modules`,
+        path.resolve('./app'),
+      ],
+      packageMains: ['webpack', 'browser', 'web', 'browserify', ['jam', 'main'], 'main'],
+    },
+
+    module: {
+      preLoaders: [
+        {
+          test: /\.jsx$|\.js$/,
+          loader: 'eslint-loader',
+          exclude: /node_modules/,
         },
-      },
-      {
-        test: /\.json$/,
-        loader: 'json-loader',
-      },
+      ],
+      loaders: [
+        {
+          test: /\.jsx$|\.js$/,
+          loader: 'babel-loader',
+          exclude: /node_modules/,
+          query: {
+            presets: ['es2015', 'react'],
+          },
+        },
+        {
+          test: /\.json$/,
+          loader: 'json-loader',
+        },
+      ],
+    },
+
+    plugins: [
+      new webpack.NoErrorsPlugin(),
+      new ExternalsPlugin('commonjs', [
+        'app',
+        'auto-updater',
+        'browser-window',
+        'content-tracing',
+        'dialog',
+        'global-shortcut',
+        'ipc',
+        'menu',
+        'menu-item',
+        'power-monitor',
+        'protocol',
+        'tray',
+        'remote',
+        'web-frame',
+        'clipboard',
+        'crash-reporter',
+        'screen',
+        'shell',
+      ]),
+      new NodeTargetPlugin(),
     ],
+    eslint: {
+      configFile: './.eslintrc.json',
+      fix: DEBUG,
+    },
+    watch: DEBUG,
   },
-
-  plugins: [
-    new webpack.NoErrorsPlugin(),
-    new ExternalsPlugin('commonjs', [
-      'app',
-      'auto-updater',
-      'browser-window',
-      'content-tracing',
-      'dialog',
-      'global-shortcut',
-      'ipc',
-      'menu',
-      'menu-item',
-      'power-monitor',
-      'protocol',
-      'tray',
-      'remote',
-      'web-frame',
-      'clipboard',
-      'crash-reporter',
-      'screen',
-      'shell',
-    ]),
-    new NodeTargetPlugin(),
-  ],
-  eslint: {
-    configFile: './.eslintrc.json',
-    fix: DEBUG,
+  // Style関連
+  {
+    cache: DEBUG,
+    context: `${__dirname}/app/assets/styles`,
+    entry: entryForStyles,
+    output: {
+      path: bundles('styles'),
+      filename: '[name].bundle.css',
+    },
+    module: {
+      loaders: [
+        {
+          test: /\.css$/,
+          loader: 'style-loader!css-loader',
+        },
+        {
+          test: /\.scss$/,
+          loader: 'style-loader!css-loader!sass-loader',
+        },
+      ],
+    },
+    watch: DEBUG,
   },
-  watch: DEBUG,
-
-};
-
-config.target = function renderer(compiler) {
-  compiler.apply(
-    new JsonpTemplatePlugin({
-      path: bundles,
-      filename: 'indexBundle.js',
-    }),
-    new FunctionModulePlugin({
-      path: bundles,
-      filename: 'indexBundle.js',
-    })
-  );
-};
+];
 
 module.exports = config;
