ded60d7badd1036a50be6cf98ad75c52203a968d
seiya kawamura <seiya.kawamura@gmail.com>
2017-01-14 16:33:56 +0900

    ディレクトリ構成を変更

---
+++ app/config/apiKey.js
@@ -0,0 +1,10 @@
+const FIREBASE_CONFIG = {
+  apiKey: 'AIzaSyD0Nr90I7O6YocucM7J4gedFqdA7PLk6mk',
+  authDomain: 'session-150811.firebaseapp.com',
+  databaseURL: 'https://session-150811.firebaseio.com',
+  storageBucket: 'session-150811.appspot.com',
+};
+
+const YOUTUBE_API_KEY = 'AIzaSyBJ31p1H_GSmmzzek1JRN0klwYB8QFl-M0';
+
+export { FIREBASE_CONFIG, YOUTUBE_API_KEY };
