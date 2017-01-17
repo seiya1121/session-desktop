b56e57446c1e21638a63af32ccdc9e324cfc6138
seiya1121 <seiya.kawamura@gmail.com>
2017-01-18 00:17:02 +0900

    一旦コミット

--- sample-darwin-x64/sample.app/Contents/Resources/app/app/components/reactBaseComponent.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default class ReactBaseComponent extends React.Component {
-  bind(...methods) {
-    methods.forEach((method) => {
-      this[method] = this[method].bind(this);
-    });
-  }
-}
--- sample-darwin-x64/sample.app/Contents/Resources/app/app/config/firebaseApp.js
+++
@@ -1,8 +0,0 @@
-import firebase from 'firebase';
-import { FIREBASE_CONFIG } from './apiKey.js';
-import Rebase from 're-base';
-
-export const firebaseApp = firebase.initializeApp(FIREBASE_CONFIG);
-export const firebaseDb = firebaseApp.database();
-export const firebaseAuth = firebaseApp.auth();
-export const base = Rebase.createClass(FIREBASE_CONFIG, 'session');
